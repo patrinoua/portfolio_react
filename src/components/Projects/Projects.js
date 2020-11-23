ab2e19100222e43186a28523d1a00e4083a99de0
Mona Lisa <patrinoua@gmail.com>
2020-11-23 12:44:36 +0100

    update projects and project text

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,9 +1,9 @@
 import React, {
   // lazy,
-  Suspense,
-} from 'react'
+  Suspense
+} from "react"
 
-import { Title, H2, RegularWithMargin, RegularCenter } from '../Typography'
+import { Title, H2, RegularWithMargin, RegularCenter } from "../Typography"
 import {
   ProjectContainer,
   ProjectImage,
@@ -13,16 +13,16 @@ import {
   VisitWebsite,
   VisitWebsiteLink,
   VisitCodeLink,
-  CodeIsPrivateMsg,
-} from './elements'
+  CodeIsPrivateMsg
+} from "./elements"
 
 const Projects = () => {
   const projects = [
-    '5_pingpongbuddies',
-    '10_Connect4',
+    "5_pingpongbuddies",
+    "10_Connect4",
     // '15_Ioanna',
-    '20_PinApp',
-    '60_klazaridis',
+    "20_PinApp",
+    "60_klazaridis"
     // '30_Imageboard',
   ]
   const listOfProjects = []
@@ -33,7 +33,7 @@ const Projects = () => {
   })
 
   return (
-    <Suspense fallback={'Loading Projects...'}>
+    <Suspense fallback={"Loading Projects..."}>
       <Title>Selected Projects</Title>
       {listOfProjects.map(
         ({
@@ -42,11 +42,13 @@ const Projects = () => {
           createdWith,
           heroku_url,
           github_url,
+          design_url = "",
+          design_tool = "",
           project,
-          codeIsAvailable,
+          codeIsAvailable
         }) => (
           <ProjectContainer key={displayName}>
-            {console.log('project', project)}
+            {console.log("project", project)}
             <ProjectImage
               href={heroku_url}
               target='_blank'
@@ -59,7 +61,7 @@ const Projects = () => {
                 </VisitWebsiteLink>
               </VisitWebsite>
               <CheckoutCode>
-                {codeIsAvailable === '1' ? (
+                {codeIsAvailable === "1" ? (
                   <VisitCodeLink href={github_url} target='_blank'>
                     See the Code
                   </VisitCodeLink>
@@ -71,44 +73,62 @@ const Projects = () => {
             <ProjectText>
               <H2>{displayName}</H2>
               <RegularCenter>{description}</RegularCenter>
+              {design_tool && (
+                <>
+                  <br />
+                  <RegularCenter>
+                    Designed with <t />
+                    {design_url ? (
+                      <a
+                        style={{ marginLeft: "5px" }}
+                        target='_blank'
+                        href={design_url}
+                      >
+                        {" "}
+                        {design_tool}
+                      </a>
+                    ) : (
+                      <p style={{ marginLeft: "5px" }}>{design_tool}</p>
+                    )}
+                    .
+                  </RegularCenter>
+                </>
+              )}
               <br />
-              <RegularCenter>{createdWith}</RegularCenter>
+              <RegularCenter>{createdWith}.</RegularCenter>
             </ProjectText>
           </ProjectContainer>
         )
       )}
       <RegularWithMargin>
-        In my free time I like to play board games, cook, travel, take{' '}
+        In my free time I like to play board games, cook, travel, take{" "}
         <a
           href='https://www.flickr.com/photos/11333811@N08/'
           target='_blank'
-          style={{ textDecoration: 'underline' }}
           rel='noopener noreferrer'
         >
-          pictures,
-        </a>{' '}
-        listen to{' '}
+          pictures
+        </a>
+        , listen to{" "}
         <a
           href='https://www.last.fm/user/MrsBlackGr'
           target='_blank'
-          style={{ textDecoration: 'underline' }}
           rel='noopener noreferrer'
         >
-          music,
-        </a>{' '}
+          music
+        </a>
+        ,
         <a
           href='https://www.dropbox.com/sh/dz7wb1inaitoryj/AABZF9fpEA7l85ZX4GlWTfOma?dl=0'
           target='_blank'
-          style={{ textDecoration: 'underline' }}
           rel='noopener noreferrer'
         >
           draw
-        </a>{' '}
-        and read{' '}
+        </a>{" "}
+        and read{" "}
         <a
           href=' https://www.goodreads.com/user/show/41608895-angeliki'
           target='_blank'
-          style={{ textDecoration: 'underline' }}
           rel='noopener noreferrer'
         >
           books
